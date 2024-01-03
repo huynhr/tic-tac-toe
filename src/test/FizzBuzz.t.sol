bacc7f9158070b0a8dc803689f2a367ceb2bdc47
Ray <huynh.r.k@gmail.com>
2024-01-02 17:50:22 -0800

    added property based testing

--- src/test/FizzBuzz.t.sol
+++ src/test/FizzBuzz.t.sol
@@ -2,35 +2,42 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "forge-std/Vm.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
+
 import "../FizzBuzz.sol";
 
-
 contract FizzBuzzTest is DSTest {
+    Vm public constant vm = Vm(HEVM_ADDRESS);
     FizzBuzz internal fizzbuzz;
 
     function setUp() public {
         fizzbuzz = new FizzBuzz();
     }
 
-    function test_returns_fizz_when_divisible_by_three() public {
-        assertEq(fizzbuzz.fizzbuzz(3), "fizz");
-        assertEq(fizzbuzz.fizzbuzz(6), "fizz");
-        assertEq(fizzbuzz.fizzbuzz(27), "fizz");
+    // property based testing
+    function test_returns_fizz_when_divisible_by_three(uint256 n) public {
+        vm.assume(n % 3 == 0);
+        vm.assume(n % 5 != 0);
+        assertEq(fizzbuzz.fizzbuzz(n), "fizz");
     }
 
-    function test_returns_buzz_when_divisible_by_five() public {
-        assertEq(fizzbuzz.fizzbuzz(5), "buzz");
-        assertEq(fizzbuzz.fizzbuzz(10), "buzz");
-        assertEq(fizzbuzz.fizzbuzz(175), "buzz");
+    function test_returns_buzz_when_divisible_by_five(uint256 n) public {
+        vm.assume(n % 3 != 0);
+        vm.assume(n % 5 == 0);
+        assertEq(fizzbuzz.fizzbuzz(n), "buzz");
     }
 
-    function test_returns_buzz_when_divisible_by_three_and_five() public {
-        assertEq(fizzbuzz.fizzbuzz(15), "fizzbuzz");
-        assertEq(fizzbuzz.fizzbuzz(30), "fizzbuzz");
-        assertEq(fizzbuzz.fizzbuzz(45), "fizzbuzz");
+    function test_returns_fizzbuzz_when_divisible_by_three_and_five(uint256 n) public {
+        vm.assume(n % 3 == 0);
+        vm.assume(n % 5 == 0);
+        assertEq(fizzbuzz.fizzbuzz(n), "fizzbuzz");
     }
 
-    function test_returns_number_when_not_divisible_by_three_or_five() public {
-        assertEq(fizzbuzz.fizzbuzz(1), "1");
+    function test_returns_number_when_not_divisible_by_three_or_five(uint256 n) public {
+        vm.assume(n % 3 != 0);
+        vm.assume(n % 5 != 0);
+
+        assertEq(fizzbuzz.fizzbuzz(n), Strings.toString(n));
     }
 }
